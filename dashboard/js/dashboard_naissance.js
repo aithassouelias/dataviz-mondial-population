44a0730eea821169f82ef6ae3e5a5342e31f4e87
Wissambelkaid <wbelkaid@gmail.com>
2023-10-17 17:31:41 +0200

    Ajout graphiques JS naissances

---
+++ dashboard/js/dashboard_naissance.js
@@ -0,0 +1,91 @@
+window.addEventListener('load', () => {
+
+  const ctx = document.getElementById('lineChart');
+
+	new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: ['2009','2010','2011','2012','2013','2014','2015','2016','2017'],
+			datasets: [
+        {
+				  label: 'Femme',
+				  data: [3000,11000,3000,10500,9000,12000,5000,2500,6500],
+          borderWidth: 1,
+				},
+				{
+				  label: 'Homme',
+				  data: [6000,4500,8000,5500,12000,14000,8000,9000,10000],
+          borderWidth: 1,
+				}
+      ]
+		},
+    options: {
+      responsive: true,
+      plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: "Evolution du nombre de naissance par année"
+      }
+      }
+    },
+  })
+  
+  const ctx1 = document.getElementById('doughnutChart');
+  new Chart(ctx1, {
+		type: 'doughnut',
+		data: {
+			labels: ['Low Income','Lower middle Income','Upper middle Income','High Income'],
+			datasets:[
+        {
+          data: [12000,10000,2000,8000],
+          borderWidth: 1,
+				}
+      ]
+		},
+    options: {
+      responsive: true,
+      plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: "Evolution du nombre de naissance par année"
+      }
+      }
+    },
+    
+  })
+  const ctx2 = document.getElementById('BarChartHorizontal');
+
+	new Chart(ctx2, {
+		type: 'bar',
+		data: {
+		labels: ['Algeria','Morocco','France','United States of America','Ivory Cost','China','Portugal','Spain','Germany','South Africa'],
+		datasets: [
+      {
+      label: 'Nombre de naissance',
+      data: [90,85,80,75,70,65,60,55,50,45],
+      borderWidth: 1,
+			},
+    ]
+		},
+    options: {
+      indexAxis: 'y',
+      responsive: true,
+      plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: "Evolution du nombre de naissance par année"
+      }
+      }
+    },
+    
+  })
+});
