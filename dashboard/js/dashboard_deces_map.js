b0ec0bc501690ea955d5c9df70814c8ee341242c
aoudia3u <nabil.aoudia54@gmail.com>
2023-10-19 19:27:16 +0200

    Avancement JS DECES

--- dashboard/js/dashboard_deces_map.js
+++ dashboard/js/dashboard_deces_map.js
@@ -1,29 +1,32 @@
-fetch('https://unpkg.com/world-atlas/countries-110m.json').then((r) => r.json()).then((data) => {
-      const countries = ChartGeo.topojson.feature(data, data.objects.countries).features;
+fetch('https://unpkg.com/world-atlas@2.0.2/countries-50m.json').then((r) => r.json()).then((data) => { 
+    const countries = ChartGeo.topojson.feature(data, data.objects.countries).features;
 
-  const chart = new Chart(document.getElementById("canvas").getContext("2d"), {
-    type: 'choropleth',
-    data: {
-      labels: countries.map((d) => d.properties.name),
-      datasets: [{
-        label: 'Countries',
-        data: countries.map((d) => ({feature: d, value: Math.random()})),
-      }]
-    },
-    options: {
-      showOutline: false,
-      showGraticule: false,
-      plugins: {
-        legend: {
-          display: false
-        },
+    const chart = document.getElementById("worldMap").getContext("2d");
+
+    new Chart(chart, {
+      type: 'choropleth',
+      data: {
+        labels: countries.map((d) => d.properties.name),
+        datasets: [{
+          label: 'Countries',
+          data: countries.map((d) => ({feature: d, value: Math.random()})),
+        }]
       },
-      scales: {
-        projection: {
-          axis: 'x',
-          projection: 'equalEarth'
+      options: { 
+        showOutline: true,
+        showGraticule: true,
+        plugins: {
+          legend: {
+            display: false
+          },
+        },
+        scales: {
+          xy : {
+            projection: {
+              projection: 'equalEarth'
+            }
+          }
         }
       }
-    }
-  });
+    });
 });
