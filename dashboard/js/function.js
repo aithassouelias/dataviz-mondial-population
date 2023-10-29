8b3aad5dc57296b1700f0017e62cce8de3a809af
Elias <aithassouelias@gmail.com>
2023-10-29 18:32:56 +0100

    Arborescence finale

--- dashboard/js/function.js
+++
@@ -1,8 +0,0 @@
-function rediriger() {
-    var listeDeroulante = document.getElementById("redirectionList");
-    var choix = listeDeroulante.value;
-    
-    if (choix !== "") {
-        window.location.href = choix; // Redirection vers la page sélectionnée
-    }
-}
