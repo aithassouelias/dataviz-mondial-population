7b3c1788b221378e01997a5313f48fc9db0c7038
Elias <aithassouelias@gmail.com>
2023-10-31 22:10:04 +0100

    Ajout des KPIs et LineChart dashboard total

--- src/controller/total/dashboard_total.js
+++ src/controller/total/dashboard_total.js
@@ -8,9 +8,10 @@ _valuesMale : solde naturel homme
 _valuesFemale : solde naturel femme
 _years : années
 */
-function createLineChart(_valuesMale, _valuesFemale, _years) {
+async function createLineChart(_birth,_death, _continent="") {
 	const ctx = document.getElementById('linechart');
-
+    const spinner = createSpinner();
+    ctx.parentNode.appendChild(spinner);
 	const existingChart = Chart.getChart(ctx);
 
 	// Supprimer le graphique si il existe afin de recréer le nouveau lorsque l'on change de filtre
@@ -18,39 +19,62 @@ function createLineChart(_valuesMale, _valuesFemale, _years) {
 		existingChart.destroy();
 	}
 
-	// Création du graphique avec les paramètres donnés en entrée de la fonction
 
-	new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: years,
-            datasets: [
-                {
-                    label: 'Solde naturel',
-                    data: soldeNaturel,
-                    borderWidth: 1
-                }
-            ]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: false
-                },
+    try {
+        const birthsByYear = await _birth.getBirthsValuesByYear();
+        const deathsByYear = await _death.getDeathsValuesByYear();
+
+        const birthsGrouped = _birth.groupValueByColumn("year", birthsByYear);
+        const deathsGrouped = _death.groupValueByColumn("year", deathsByYear);
+
+        const years = Object.keys(birthsGrouped);
+
+        const soldeNaturel = years.map((year) => {
+            const births = birthsGrouped[year] || 0;
+            const deaths = deathsGrouped[year] || 0;
+            return births - deaths;
+        });
+
+        spinner.remove();
+        // Création du graphique avec les paramètres donnés en entrée de la fonction
+
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: years,
+                datasets: [
+                    {
+                        label: 'Solde naturel',
+                        data: soldeNaturel,
+                        borderWidth: 1
+                    }
+                ]
             },
-            responsive: true,
-            plugins: {
-                legend: {
-                    display: false,
-                    position: 'top',
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: false
+                    },
                 },
-                title: {
-                    display: true,
-                    text: "Evolution du solde naturel par année"
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false,
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: "Evolution du solde naturel par année"
+                    }
                 }
             }
-        }
-    });
+        });
+        
+    } catch (error) {
+        console.error('Erreur lors de la récupération ou du traitement des données :', error);
+    }
+
+	
 }
 
 
@@ -75,7 +99,8 @@ function _getKPIValues(_death, _birth,_continent="") {
 	let functionToCall = _birth.getBirthsValues().then((births) => {
 
             // KPI NAISSANCES
-            KPITotalBirth = birth.groupValues(births);
+            KPITotalBirth = _birth.groupValues(births);
+            spinner.remove();
             const ctx = document.getElementById('KPI-Total-Naissances');
             ctx.textContent = KPITotalBirth.toLocaleString();
 
@@ -88,7 +113,8 @@ function _getKPIValues(_death, _birth,_continent="") {
             functionToCall = _birth.getBirthsValuesContinent(_continent).then((births) => {
 
             // KPI NAISSANCES
-            KPITotalBirth = birth.groupValues(births);
+            KPITotalBirth = _birth.groupValues(births);
+            spinner.remove();
             const ctx = document.getElementById('KPI-Total-Naissances');
             ctx.textContent = KPITotalBirth.toLocaleString();
 
@@ -101,7 +127,7 @@ function _getKPIValues(_death, _birth,_continent="") {
     let KPITotalBirth, KPITotalDeath;
     
     functionToCall.then((deaths) => {
-
+    
         // KPI DECES
         KPITotalDeath = _death.groupValues(deaths);
         const ctx = document.getElementById('KPI-Total-Deces');
@@ -123,6 +149,10 @@ function initializeData(continent="") {
 	const death = new Death();
     const birth = new Birth();
 
+    // Affichage des KPIs
 	_getKPIValues(death,birth,continent)
 
+    // Affichage du Line Chart
+    createLineChart(birth,death,continent)
+    
 }
